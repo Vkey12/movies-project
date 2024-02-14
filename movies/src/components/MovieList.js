360f0d957a2834e4285b3479f405eb7f055009a9
mayarrrrrr <akokmayar607@gmail.com>
2024-02-14 12:01:53 +0300

    Display of the movie's poster

--- movies/src/components/MovieList.js
+++ movies/src/components/MovieList.js
@@ -2,4 +2,24 @@
 
 const MovieList = (props) => {
     const FavouriteComponent = props.favourite;
+
+    return (
+        <>
+        {props.movies.map((movie,index)=>(
+            <div className="image-container d-flex justify-content-start m-3">
+                <img src={movie.Poster} alt='movie'></img>
+                
+
+
+
+
+
+            </div>
+
+
+
+        ))}
+        
+        </>
+    )
 }
