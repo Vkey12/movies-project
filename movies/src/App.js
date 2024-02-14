c773610d0a0a18b620e725e931c661e858d24d28
victor <victor.odhi@gmail.com>
2024-02-14 11:55:48 +0300

    fetched

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+const App = () => {
+  const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `https://movies-two-theta.vercel.app/Search`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+  };
 }
 
 export default App;
