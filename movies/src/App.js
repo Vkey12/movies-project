8d34bb8d40a3a12d3a13e23104213a3e6803f48a
Vkey12 <68274999+Vkey12@users.noreply.github.com>
2024-02-14 13:00:06 +0300

    Update App.js

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBox from './components/SearchBox';
+
 const App = () => {
   const [movies, setMovies] = useState([]);
 	const [favourites, setFavourites] = useState([]);
@@ -16,6 +17,37 @@ const App = () => {
 			setMovies(responseJson.Search);
 		}
   };
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+         const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 }
 
 export default App;
