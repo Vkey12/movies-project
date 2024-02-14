3a00beb1a0696039e7daaa463ae2a5b406ab4f3b
Ob55 <brian55mwangi@gmail.com>
2024-02-14 12:03:20 +0300

    importing RemoveFavourite

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AddFavourite from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 function App() {
   return (
     <div className="App">
